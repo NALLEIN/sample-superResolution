81eda8cef13bdb7beca064ea9bc2f4c1b8f27248
NALLEIN <hao1.jiang@intel.com>
2020-01-10 16:51:24 +0800

    Add image measure metrics

--- src/imageMetrics.py
+++ src/imageMetrics.py
@@ -5,12 +5,13 @@ import scipy.ndimage
 from imageio import imread
 from numpy.ma.core import exp
 from scipy.constants.constants import pi
-
+from skimage.measure import compare_ssim  #skimage version 0.16
+from skimage.measure import compare_psnr  #skimage version 0.16
 
 def get_args():
     parser = argparse.ArgumentParser(
         conflict_handler='resolve',
-        description='eg: python3 -img1 file1 -img2 file1 -m 1' )
+        description='eg: python3 -img1 file1 -img2 file1 -m 1 -c 0' )
     parser.add_argument('-img1','--image_1',required=True,
                         help='image file_1 URL')
     parser.add_argument('-img2','--image_2',required=True,
@@ -18,26 +19,57 @@ def get_args():
     parser.add_argument('-m','--metric',required=True,type = int,
                         help='metric method\
                                 0: PSNR ,1:SSIM ')
+    parser.add_argument('-c','--isColored',required=True,type = int,
+                        help= 'weather the input img is colored')
     return parser.parse_args()
+
+def RGB2YUV( rgb ):
+     
+    m = np.array([[ 0.29900, -0.16874,  0.50000],
+                 [0.58700, -0.33126, -0.41869],
+                 [ 0.11400, 0.50000, -0.08131]])
+     
+    yuv = np.dot(rgb,m)
+    yuv[:,:,1:]+=128.0
+    return yuv
+
+def YUV2RGB( yuv ):
+      
+    m = np.array([[ 1.0, 1.0, 1.0],
+                 [-0.000007154783816076815, -0.3441331386566162, 1.7720025777816772],
+                 [ 1.4019975662231445, -0.7141380310058594 , 0.00001542569043522235] ])
     
-def psnr(img1, img2):
+    rgb = np.dot(yuv,m)
+    rgb[:,:,0]-=179.45477266423404
+    rgb[:,:,1]+=135.45870971679688
+    rgb[:,:,2]-=226.8183044444304
+    rgb =rgb.clip(0,255)
+    return rgb
+
+def readimage(img1,img2,isColored):
     im1_data = imread(img1)
     #im1_data = im1_data[scale[0]:-scale[0],scale[1]:-scale[1]]
     im1_data = im1_data.astype(np.float)
+    if(isColored != 0):
+        im1_data = RGB2YUV(im1_data)
+        im1_data = im1_data[:,:,0]
     im2_data = imread(img2)
     #im2_data = im2_data[scale[0]:-scale[0],scale[1]:-scale[1]]
     im2_data = im2_data.astype(np.float)
+    if(isColored != 0):
+        im2_data = RGB2YUV(im2_data)
+        im2_data = im2_data[:,:,0]
+    return [im1_data,im2_data]
+
+def psnr(img1, img2, isColored):
+    [im1_data,im2_data] = readimage(img1,img2,isColored)
+    #return compare_psnr(im1_data,im2_data)
     diff = im1_data  - im2_data 
     mse = np.mean(diff ** 2)
     return 10 * math.log10(255.0**2/mse)
 
-def ssim(img1, img2):
-    im1_data = imread(img1)
-    #im1_data = im1_data[scale[0]:-scale[0],scale[1]:-scale[1]]
-    im1_data = im1_data.astype(np.float)
-    im2_data = imread(img2)
-    #im2_data = im2_data[scale[0]:-scale[0],scale[1]:-scale[1]]
-    im2_data = im2_data.astype(np.float)
+def ssimnaive(img1, img2,isColored):
+    [im1_data,im2_data] = readimage(img1,img2,isColored)
     #Variables for Gaussian kernel definition
     gaussian_kernel_sigma=1.5
     gaussian_kernel_width=11
@@ -89,12 +121,15 @@ def ssim(img1, img2):
 
     return index
 
+def ssim(img1, img2,isColored):
+    [im1_data,im2_data] = readimage(img1,img2,isColored)
+    return compare_ssim(im1_data,im2_data)
 def main():
     args = get_args()
     if(args.metric == 0) :
-        print(psnr(args.image_1,args.image_2))
+        print(psnr(args.image_1,args.image_2,args.isColored))
     elif(args.metric == 1) :
-        print(ssim(args.image_1,args.image_2))
+        print(ssim(args.image_1,args.image_2,args.isColored))
 
 if __name__ == '__main__':
     main()
